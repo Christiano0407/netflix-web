0cae4a2af3a4da673083ddb22dd095c7ab3a97f2
Christiano0407 <placebo0704@gmail.com>
2020-04-16 12:51:59 -0500

    carousel2

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+const fila = document.querySelector(`.contenedor-carousel`);
+const pelicula = document.querySelector(`.pelicula`);
+
+const izquierda = document.getElementById(`flecha-izq`);
+const derecha = document.getElementById(`flecha-der`);
