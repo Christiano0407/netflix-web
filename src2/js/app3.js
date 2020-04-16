290a8850aa4447087a950f42da15cecd2f187b0c
Christiano0407 <placebo0704@gmail.com>
2020-04-16 07:23:00 -0500

    heart

---
+++ src2/js/app3.js
@@ -0,0 +1,9 @@
+/* const $like = document.getElementById(`like`);
+$like.addEventListener(`click`, (event) => {
+ $like.classList.add(`is-like`);
+}); */
+const $like = document.getElementById(`like`);
+
+$like.addEventListener(`click`, (event) => {
+   $like.classList.add(`is-like`);
+});
