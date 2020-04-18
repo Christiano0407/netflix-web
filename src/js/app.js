ad90678b3cdaab9c242de81fbeb0e2bcbf1e8f26
Christiano0407 <placebo0704@gmail.com>
2020-04-18 13:23:42 -0500

    indicadores

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 const fila = document.querySelector(`.contenedor-carousel`);
-const pelicula = document.querySelector(`.pelicula`);
+const pelicula = document.querySelectorAll(`.pelicula`);
 
 const flechaIzquierda = document.getElementById(`flecha-izq`);
 const flechaDerecha = document.getElementById(`flecha-der`);
@@ -10,3 +10,21 @@ flechaDerecha.addEventListener(`click`, () => {
 flechaIzquierda.addEventListener(`click`, () => {
     fila .scrollLeft -= fila.offsetWidth;
 })
+
+/* paginación */
+const numeroPagina = Math.ceil(pelicula.length / 5);
+for(let i = 0; i < numeroPagina; i++){
+    const indicador = document.createElement(`button`);
+
+     if (i === 0){
+         indicador.classList.add(`activo`)
+     }
+    document.querySelector(`.indicadores`).appendChild(indicador);
+    indicador.addEventListener(`click`, (e) => {
+    fila.scrollLeft = i * fila.offsetWidth;
+    /* multiplicamos el anocho * 2 */
+    document.querySelector(`.indicadores .activo`).classList.remove(`activo`);
+    e.target.classList.add(`activo`);
+  /*   target=click que estamos dando */
+    })
+}
