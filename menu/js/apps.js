b1135054b42f9beb93903d04544e109d81523645
Christiano0407 <placebo0704@gmail.com>
2020-05-06 12:18:45 -0500

    finish

--- menu/js/apps.js
+++ menu/js/apps.js
@@ -1,1 +1,9 @@
-let rellax = new Rellax (`.rellax`);
+let rellax = new Rellax (`.rellax`); 
+/* var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  }); */
