97d158742aacfda382f7c3a88bdebba632bc646f
Christiano0407 <placebo0704@gmail.com>
2020-05-12 08:17:01 -0500

    interval

---
+++ slider/app.js
@@ -0,0 +1,29 @@
+const slider = document.querySelectorAll(`.slide`);
+const next = document.querySelector(`#next`);
+const prev = document.querySelector(`#prev`);
+const intervalTime = 5000;
+let SlideInterval;
+
+/* funciones */
+
+const nextSlide = () =>  {
+    const current = document.querySelector(`.current`);
+
+    current.classList.remove(`current`);
+
+    if(current.nextElementSibling){
+        current.nextElementSibling.classList.add(`current`)
+    }else {
+        slides[0].classList.add(`current`)
+    }
+    setTimeout(() => current.classList.remove(`current`));
+}
+/* 
+EVENTS/CLICK */
+next.addEventListener(`click`, e => {
+    nextSlide();
+    if(auto){
+        clearInterval(SlideInterval);
+        SlideInterval = setInterval(nextSlide, intervalTime);
+    }
+})
